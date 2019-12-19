9202118c56d9d23c19fd5d5d55303d9aa0af5d26
bjguofangfang <bjguofangfang@corp.netease.com>
2019-12-19 21:31:34 +0800

    load_dict done.

--- simple/load_dict.py
+++ simple/load_dict.py
@@ -13,24 +13,33 @@ def load_field_idx_dict(dict_file):
                 continue
             field_name = line_arr[0]
             idx_dict_str = line_arr[2]
-            idx_dict = json.loads(idx_dict_str, encoding='utf-8')
-            field_idx_dict[filed_name] = idx_dict
+            try:
+                idx_dict = json.loads(idx_dict_str, encoding='utf-8')
+            except Exception as e:
+                print (e)
+                print (idx_dict_str)
+                print (field_name)
+                continue
+            field_idx_dict[field_name] = idx_dict
     return field_idx_dict
 
 #def read_pretrain_table_file_use_statistics(table_file_name: str, dim: int, offset:int, key_size: int, table_size: int, index_dict:dict, weight_name:str):
+table_cache = {}
 def read_pretrain_table_file_use_statistics(table_file_name, dim, offset, key_size, table_size, index_dict, weight_name):
     ### np.ndarray # index_dict = {'key': value}
     import struct
     #if table_file_name in pretrain_table_cache:
     #    return pretrain_table_cache[table_file_name]
     table = np.ndarray([table_size, dim], np.float32)
+    print (table)
+    hadoop_bin="/home/recsys/platform/hadoop-jd-2.7.3/bin/hadoop"
     valid = []
     if table_file_name.startswith("hdfs://"):
         tag = table_file_name.split("/")[-1]
         #local_file_name = str(mmh3.hash(table_file_name)) + '.table'
         local_file_name = tag + '.table'
         tf.logging.info(table_file_name)
-        ##os.system(f'rm -rf {local_file_name} && hadoop fs -get {table_file_name} {local_file_name}')
+        os.system(f'rm -rf {local_file_name} && {hadoop_bin} fs -get {table_file_name} {local_file_name}')
     else:
         local_file_name = table_file_name
     n_hit = 0
@@ -63,5 +72,30 @@ def read_pretrain_table_file_use_statistics(table_file_name, dim, offset, key_si
 
 
 if __name__ == "__main__":
-    field_dict = load_field_idx_dict("./all_dict")
-    print (field_dict['2121_WordCount'])
+    field_dict = load_field_idx_dict("./2121_dict")
+    #print ( '2121_WordCount\t' +str(len(field_dict['2121_WordCount']))  + "\t" + json.dumps(field_dict['2121_WordCount'], ensure_ascii=False))
+    table_file_name= "hdfs://hz-cluster9/user/datacenter/mlp/deep/fm_vec/20191214/cat"
+    table_file_name ="hdfs://hz-cluster9/user/portal/ODM/RECOMMEND/datacenter/dnn_model/test_gff/fmvec/20191214/cat"
+    table, idxtable = read_pretrain_table_file_use_statistics(table_file_name, 64, 1, 96, 231, field_dict['2121_WordCount'], 'u_cat2')
+    print (table)
+    print (idxtable)
+    
+    valid = [1,131,53,131,131]
+    #idxtable = tf.contrib.lookup.HashTable(tf.contrib.lookup.KeyValueTensorInitializer(valid, valid), 0)
+    
+    input_tensor = tf.constant([[0,1,2,131,53],[4,5,6,7,8]], dtype=tf.int32)
+    not_valid_tensor = tf.constant(2, dtype=tf.int32)
+    out = idxtable.lookup(input_tensor)
+    out_0 = idxtable.lookup(not_valid_tensor)
+    with tf.Session() as sess:
+        idxtable.init.run()
+        print (out)
+        print(out.eval())  ### 1
+        hit_rate = tf.divide(tf.count_nonzero(out, dtype=tf.int32), tf.size(out), name='test')
+        print (hit_rate)
+        print (sess.run(hit_rate))
+        print (tf.count_nonzero(out, dtype=tf.int32))
+        print (sess.run(tf.count_nonzero(out, dtype=tf.int32)))
+        print (tf.size(out))
+        print (sess.run(tf.size(out)))
+
